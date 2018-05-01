9a36882f2c288ced06448d73823d46d03484fca1
carsenk <carsenk@gmail.com>
2018-04-30 19:25:14 -0600

    Changes for DNR

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
-var defaultCurrency = localStorage.getItem('insight-currency') || 'XPY';
+var defaultCurrency = localStorage.getItem('insight-currency') || 'DNR';
 
 angular.module('insight',[
   'ngAnimate',
